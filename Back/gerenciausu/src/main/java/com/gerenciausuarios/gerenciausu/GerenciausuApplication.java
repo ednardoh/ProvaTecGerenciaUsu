01f7c65c6496142a2331fbae4e0a2dfeaaaf1276
ednardo honorato <ednardoh@gmail.com>
2024-06-18 17:46:13 -0300

    adicionando projeto provaTecGerenciaUsu no git - ednardo honorato 18-06-2024

---
+++ Back/gerenciausu/src/main/java/com/gerenciausuarios/gerenciausu/GerenciausuApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciausuarios.gerenciausu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciausuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciausuApplication.class, args);
+	}
+
+}
---
+++ Back/gerenciausu/src/test/java/com/gerenciausuarios/gerenciausu/GerenciausuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciausuarios.gerenciausu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciausuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
